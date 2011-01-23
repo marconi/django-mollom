df4621c1a7627f9fd579c4d721afdbaf1f5688c7
Marconi Moreto <caketoad@gmail.com>
2011-01-23 16:51:22 +0800

    Added ez_setup.py and updated README file.

---
+++ setup.py
@@ -0,0 +1,14 @@
+import ez_setup
+ez_setup.use_setuptools()
+from setuptools import setup, find_packages
+
+setup(
+    name = "django-mollom",
+    version = "0.1",
+    packages = find_packages(),
+    author = "Marconi Moreto",
+    author_email = "caketoad@gmail.com",
+    description = "A Django wrapper app for the PyMollom module.",
+    url = "https://github.com/marconi/django-mollom/",
+    include_package_data = True
+)
